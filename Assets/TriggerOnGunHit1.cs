7b55eaca7a462638551afcc8d3ba9e64a42c388a
wenchaoliu@foxmail.com <liuwenchao19>
2020-08-27 19:51:29 +0800

    trigger eventaction

---
+++ Assets/TriggerOnGunHit1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnGunHit1 : AkTriggerBase
+
+{
+
+    public void Hit()
+
+    {
+
+        if(triggerDelegate != null) 
+
+        {
+
+            triggerDelegate(null);
+
+        }
+
+    }
+
+}
